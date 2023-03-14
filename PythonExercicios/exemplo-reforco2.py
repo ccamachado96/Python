806a6096b87356e57eb809a1f45c8667bd0aa15e
Caio Machado <74932392+ccamachado96@users.noreply.github.com>
2023-03-14 11:40:41 -0300

    Adição do exemplo-reforco2

---
+++ PythonExercicios/exemplo-reforco2.py
@@ -0,0 +1,6 @@
+n1 = int(input('Informe um número: '))
+n2 = int(input('Informe mais um número: '))
+n3 = int(input('Informe um último número para fazermos a soma: '))
+
+soma = n1 + n2 + n3
+print('A soma de {} + {} + {}, é igual a: {}'.format(n1, n2, n3, soma))
