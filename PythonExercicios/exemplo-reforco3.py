97b9bb3a4497afc4eec2c3045daea1ba507a7502
Caio Machado <74932392+ccamachado96@users.noreply.github.com>
2023-03-15 20:13:52 -0300

    Exemplo-reforco3 adicionado ao repositório

---
+++ PythonExercicios/exemplo-reforco3.py
@@ -0,0 +1,9 @@
+n1 = int(input('Informe um valor qualquer: '))
+n2 = int(input('Informe mais um valor: '))
+n3 = int(input('Informe um terceiro valor: '))
+
+print('Os valores digitados foram: {}, {} e {}'.format(n1, n2, n3))
+
+soma = n1 + n2 + n3
+
+print('A soma dos valores digitados: {} + {} + {} = {}'.format(n1, n2, n3, soma))
