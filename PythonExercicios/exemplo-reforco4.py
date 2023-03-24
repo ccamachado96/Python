ddb2d7134e8cffe310bcea594f2e0d8d76d42c80
Caio Machado <74932392+ccamachado96@users.noreply.github.com>
2023-03-24 14:29:32 -0300

    Aidiconado novo exercicio de reforço (soma)

---
+++ PythonExercicios/exemplo-reforco4.py
@@ -0,0 +1,9 @@
+n1 = int(input('Informe um número: '))
+print('O número informado foi: {}'.format(n1))
+n2 = int(input('Informe mais um número: '))
+print('O número informadoi foi: {}'.format(n2))
+
+soma = n1 + n2
+
+print('A soma de {} + {} = {}'.format(n1, n2, soma))
+
