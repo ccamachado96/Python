6f765b62d312c8abcde4c4b2c382a3bfa0c59948
Caio Machado <74932392+ccamachado96@users.noreply.github.com>
2023-03-26 17:31:21 -0300

    ex003 do Gustavo Guanabara, somando dois números

---
+++ PythonExercicios/ex003.py
@@ -0,0 +1,13 @@
+#Exercício 003 da lista do Gustavo Guanabara
+#Somando dois números
+
+n1 = int(input('Informe um número: '))
+n2 = int(input('Informe mais um número: '))
+
+print('Os números digitados foram: {} e {}.'.format(n1, n2))
+
+soma = n1 + n2
+
+print('A soma de {} + {} equivale a: {}'.format(n1, n2, soma))
+
+
