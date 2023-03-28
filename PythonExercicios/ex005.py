0384219af4ffd5b36a3b1cbdfb9427aa6674c465
Caio Machado <74932392+ccamachado96@users.noreply.github.com>
2023-03-28 13:09:10 -0300

    Calculando antecessor e sucessor

--- PythonExercicios/ex005.py
+++ PythonExercicios/ex005.py
@@ -1,4 +1,5 @@
 #Calculando o Antecessor e Sucessor de um número
+#1ª forma
 
 ante = int(input('Informe um número: '))
 print('O número digitado foi: {}'.format(ante))
@@ -9,3 +10,11 @@ print('O antecessor de {}, equivale a: {}'.format(ante, antecessor))
 print('O sucessor de {}, equivale a: {}'.format(ante, sucessor))
 
 
+
+
+#2ª forma
+
+n = int(input('Informe um número: '))
+a = n - 1
+s = n + 1
+print('O antecessor de {} é {}, e o sucessor é {}'.format(n, a, s))
