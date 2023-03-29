ead4e2584bfb464aa548f2c7590af8f7b6637d3e
Caio Machado <74932392+ccamachado96@users.noreply.github.com>
2023-03-29 09:01:06 -0300

    ex006 - Dobro, triplo e raiz

---
+++ PythonExercicios/ex006.py
@@ -0,0 +1,12 @@
+n = int(input('Informe um número qualquer: '))
+
+print('O número informado foi: {}'.format(n))
+
+dobro = n*2
+triplo = n*3
+#raiz = n/2
+
+print('O dobro de {}, vale {}'.format(n, dobro))
+print('O triplo de {}, vale {}'.format(n, triplo))
+#print('A raiz de {}, vale {}'.format(n, raiz))
+
