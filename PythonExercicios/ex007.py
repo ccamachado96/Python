e0f90bb7526843b59714c6f6dd6d505c726e71ce
Caio Machado <74932392+ccamachado96@users.noreply.github.com>
2023-03-29 10:45:05 -0300

    ex007 - Média Aritmética

---
+++ PythonExercicios/ex007.py
@@ -0,0 +1,9 @@
+#Média Aritmética - ex007 Gustavo Guanabara
+
+n1 = float(input('Informe a primeira nota do aluno: '))
+n2 = float(input('Informe a segunda nota do aluno: '))
+
+media = n1 + n2 / 2
+
+print('A média de notas do aluno foi: {:.2f}'.format(media))
+
