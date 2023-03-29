acb64d4f634a328153b16cbeb861905d8c89418e
Caio Machado <74932392+ccamachado96@users.noreply.github.com>
2023-03-29 09:16:42 -0300

    ex006 - Dobro, triplo e raiz

--- PythonExercicios/ex006.py
+++ PythonExercicios/ex006.py
@@ -8,5 +8,25 @@ raiz = n ** (1/2)
 
 print('O dobro de {}, vale {}'.format(n, dobro))
 print('O triplo de {}, vale {}'.format(n, triplo))
-print('A raiz de {}, vale {}'.format(n, raiz))
+print('A raiz de {}, vale {:.2f}'.format(n, raiz))
 
+
+#Comentários
+
+# \n é usado para quebrar a linha
+# ** (1/2) - Para calcular a raiz quadrada. Necessário colocar o parenteses devido a ordem de precedência
+# :.2f - usado para formatar melhor os números quebrados - 2 significa o número de cada decimais após a vírgula
+
+
+
+
+
+# 2ª forma:
+
+n = int(input('Informe um número qualquer: '))
+
+print('O número informado foi: {}'.format(n))
+
+print('O dobro de {}, vale {}'.format(n, (n*2)))
+print('O triplo de {}, vale {}'.format(n, (n*3)))
+print('A raiz de {}, vale {:.2f}'.format(n, (n ** (1/2))))
