63317e83240d4d5db3f7094006abd020a7fc9762
Caio Machado <74932392+ccamachado96@users.noreply.github.com>
2023-03-30 07:58:07 -0300

    ex007 - Calculando a média de um aluno

--- PythonExercicios/ex007.py
+++ PythonExercicios/ex007.py
@@ -2,8 +2,10 @@
 
 n1 = float(input('Informe a primeira nota do aluno: '))
 n2 = float(input('Informe a segunda nota do aluno: '))
+n3 = float(input('Informe a terceira nota do aluno: '))
 
-media = n1 + n2 / 2
+
+media = (n1 + n2+ n3) / 3
 
 print('A média de notas do aluno foi: {:.2f}'.format(media))
 
