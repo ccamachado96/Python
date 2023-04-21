ea609382011ddd580078abb60c9eb1674cafd7ef
Caio Machado <74932392+ccamachado96@users.noreply.github.com>
2023-04-21 14:58:52 -0300

    Exercicio feito na aula 08Usando biblioteca Random

---
+++ PythonExercicios/aula08b.py
@@ -0,0 +1,26 @@
+#Gera um número float, real, entre 0 e 1
+
+#Usando a biblioteca random para gerar um número aleatório entre 0 e 1
+
+#1ª forma
+
+import random
+
+num = random.random()
+print(num)
+
+
+
+
+#-------------------------------------------------------------------------------
+
+
+
+#2ª forma - Randomizando um número inteiro, com a função de randint
+
+import random
+num = random.randint(1, 10)          #Escolherá nesta linha, um número de 1 até 10
+print(num)
+
+
+
---
+++ PythonExercicios/aula08c.py
@@ -0,0 +1,19 @@
+#Aula08c - Usando biblioteca Emoji
+
+
+#import emoji
+#print(emoji.emojize('Olá, mundo :smile:', language = 'en'))
+
+
+
+#-------------------------------------------------------------------------
+
+
+#import emoji
+#print(emoji.emojize("Olá Programador! :rocket:"))
+
+
+
+
+import emoji
+print(emoji.emojize('Python is :thumbs_up:'))
