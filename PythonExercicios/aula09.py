ac12664baab0d7102f11f67be1f1e0457b03ddc1
Caio Machado <74932392+ccamachado96@users.noreply.github.com>
2023-04-22 14:47:06 -0300

    #Manipulando texto - Aula 09

---
+++ PythonExercicios/aula09.py
@@ -0,0 +1,30 @@
+#Manipulando texto - Aula 09
+
+
+frase = 'Curso em Vídeo Python'
+print(frase[:14])
+
+
+#Utilize as trÊs aspas para imprimir um texto longo na tela de uma vez só
+
+
+print("""laoskoaskasoaksaoskoaskasoaksaoskoaskasoaksaoskoaskasoaksaoskoaskasoaksaoskoaskasoaksosakso
+aoskoaskasoaksaoskoaskasoaksaoskoaskasoaksaoskoaskasoaksaoskoaskasoaksaoskoaskasoaksaoskoaskasoaks
+aoskoaskasoaksaoskoaskasoaksaoskoaskasoaksaoskoaskasoaksaoskoaskasoaksaoskoaskasoaksaoskoaskasoaks
+aoskoaskasoaksaoskoaskasoaksaoskoaskasoaksaoskoaskasoaksaoskoaskasoaksaoskoaskasoaksaoskoaskasoaks
+aoskoaskasoaksaoskoaskasoaksaoskoaskasoaksaoskoaskasoaksaoskoaskasoaksaoskoaskasoaksaoskoaskasoaks""")
+
+
+
+print(frase.count('o'))
+print(len(frase))
+
+print(frase.replace('Curso', 'Caio'))
+
+print(frase)
+
+
+print('Curso' in frase)
+
+print(frase.split())
+
