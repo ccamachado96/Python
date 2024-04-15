5b2b35d772e14b6351df9ced6a9a2d0f97748a64
Caio Machado <74932392+ccamachado96@users.noreply.github.com>
2024-04-15 19:54:04 -0300

    Create ex022.py

---
+++ PythonExercicios/ex022.py
@@ -0,0 +1,30 @@
+nome = str(input('Informe seu nome por favor: '))
+
+print('O nome digitado foi: {}'.format(nome))
+
+print('O nome com todas as letras maiúsculas vale: \n{}'.format(nome.upper()))
+
+print('O nome com todas as letras em minúsculo vale: \n {}'.format(nome.lower()))
+
+print('O tamanho total de nome vale {}'.format(len(nome)))
+
+print('O nome sem considerar os espaços vale: {}'.format(len(nome.strip)))
+
+
+
+
+
+
+
+
+
+#tamanho = len(nome.strip())
+
+#print('O nome {} possui {} letras, SEM contar os espaços.'.format(nome, tamanho))
+
+#tamanho_total = len(nome)
+
+#print('O nome {} possui {}, CONSIDERANDO todos os espaços.'.format(nome, tamanho_total))
+
+
+
