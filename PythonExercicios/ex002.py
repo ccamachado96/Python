50635542be0b1d87ba19591baaa0021699ed002b
Caio Machado <74932392+ccamachado96@users.noreply.github.com>
2024-04-15 20:20:24 -0300

    ex002
    Pergunte o nome do usuário e exiba uma mensagem de boas vindas

---
+++ PythonExercicios/ex002.py
@@ -0,0 +1,6 @@
+#ex002 - Pergunte o nome do usuário e exiba uma mensagem de boas vindas na tela
+
+nome = input('Informe seu nome: ')
+print('Seja bem-vindo ao novo sistema, sr {}.'.format(nome))
+
+#Feito em 15/04/2024 às 20h18
