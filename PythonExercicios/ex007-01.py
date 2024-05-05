b8599167a533ae4d6ad3161cbd8babfabc8eedce
Caio Machado <74932392+ccamachado96@users.noreply.github.com>
2024-05-05 20:19:05 -0300

    ex007-01 - Média aritmética
    ex007-01 - Média aritmética - Revisado em 0505/2024

---
+++ PythonExercicios/ex007-01.py
@@ -0,0 +1,12 @@
+#Calculando a média aritmética de um aluno - Feito em 05/05/2024 - ÀS 20H18
+
+n1 = float(input('Informe a nota do aluno: '))
+n2 = float(input('Informe a segunda nota do aluno: '))
+
+print('A primeira nota informada foi: {:.1f}'.format(n1))
+print('A segunda nota informada foi: {:.1f}'.format(n2))
+
+media = (n1 + n2) / 2
+
+print('A média do aluno foi de: {:.1f}'.format(media))
+
